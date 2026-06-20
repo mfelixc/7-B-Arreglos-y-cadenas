47c73554b1c140fda2c3071dfe94586d7540539f
mfelixc <mfelixc@unjbg.edu.pe>
2026-06-20 18:08:41 -0500

    Use un for que recorre a los n alumnos, lee el número del profesor favorito de cada uno (F) y aumenta en 1 el contador correspondiente en el arreglo frecuencia.

--- frecuencias.cpp
+++ frecuencias.cpp
@@ -5,8 +5,12 @@ int main() {
     int P, n;
     cin >> P;
     cin >> n;
-    
+    // Aqui incialice todos los valores del arreglo a 0 para evitar datos basura
     int frecuencia[101] = {0};
-
+    for (int i = 0; i < n; i++) {
+        int F;
+        cin >> F;
+        frecuencia[F] = frecuencia[F] + 1 ;
+    }
     return 0;
 }
